549eb04fcd2a7fec4324a2abee51555927b23bd6
s0rry <firebat2@mail.ru>
2022-07-04 16:55:07 +0300

    Update get_string_session.py

--- get_string_session.py
+++ get_string_session.py
@@ -12,5 +12,5 @@ load_dotenv()
 with TelegramClient(StringSession(), os.getenv("TELEGRAM_API_ID"), os.getenv("TELEGRAM_API_HASH")) as client:
     print(client.session.save())
     # Write changes to .env file. DO NOT TEST need testing :)
-    os.environ["TELEGRAM_SESSION_STRING"] = client.session.save()
-    dotenv.set_key(os.environ["TELEGRAM_SESSION_STRING"])
+    os.environ["TELEGRAM_STRING_SESSION"] = client.session.save()
+    dotenv.set_key(os.environ["TELEGRAM_STRING_SESSION"])
