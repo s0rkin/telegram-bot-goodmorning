a8771cd3fbf7f5b0edb17ac810ee695550eeee95
s0rry <firebat2@mail.ru>
2022-07-04 16:54:33 +0300

    Update get_string_session.py

--- get_string_session.py
+++ get_string_session.py
@@ -2,7 +2,7 @@
 
 import os
 
-from dotenv import load_dotenv
+import dotenv
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
@@ -11,3 +11,6 @@ load_dotenv()
 
 with TelegramClient(StringSession(), os.getenv("TELEGRAM_API_ID"), os.getenv("TELEGRAM_API_HASH")) as client:
     print(client.session.save())
+    # Write changes to .env file. DO NOT TEST need testing :)
+    os.environ["TELEGRAM_SESSION_STRING"] = client.session.save()
+    dotenv.set_key(os.environ["TELEGRAM_SESSION_STRING"])
