abf26c0c41c5bb7c476e5e7af8775426b44419bb
s0rry <firebat2@mail.ru>
2023-11-15 10:32:46 +0300

    Update calendar.py
    fix return

--- modules/calendar.py
+++ modules/calendar.py
@@ -34,6 +34,15 @@ def get_day(num_retries = 10):
         try:
             r = requests.get(os.getenv("CALENDAR_URL"), headers = header, params = param)
             t = json.loads(r.text)
+            #return full string for telegram "text" + get_day()
+            if t["work"] == "1":
+                return "Доброе утро!☝ Сегодня " + t["type"] + "."
+            elif t["work"] == "0" and (t["zag"] == "Календарный выходной день" or t["zag"] == "Перенесенный день"):
+                return "Доброе утро!☝ Сегодня " + t["type"] + "."
+            elif t["work"] == "0":
+                return "Доброе утро!☝ Сегодня " + t["type"] + ". " + t["zag"] + "."
+            else:
+                return "Доброе утро!☝ Сегодня ХЗ какой день."
         except:
             if attempt_no < (num_retries - 1):
                 time.sleep(30) #wait 30sec for api response if have error. DONT SPAM!
@@ -43,12 +52,3 @@ def get_day(num_retries = 10):
                 print("API (get_day) ERROR! 10 retries expired!")
                 break
             return "Доброе утро!☝ Сегодня ХЗ какой день. get_day API ERROR! 10 retries expired!"
-        #return full string for telegram "text" + get_day()
-        if t["work"] == "1":
-            return "Доброе утро!☝ Сегодня " + t["type"] + "."
-        elif t["work"] == "0" and (t["zag"] == "Календарный выходной день" or t["zag"] == "Перенесенный день"):
-            return "Доброе утро!☝ Сегодня " + t["type"] + "."
-        elif t["work"] == "0":
-            return "Доброе утро!☝ Сегодня " + t["type"] + ". " + t["zag"] + "."
-        else:
-            return "Доброе утро!☝ Сегодня ХЗ какой день."
