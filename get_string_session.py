04f6a531898235351a4ab5eb9afb34881a908cd0
Your Name <you@example.com>
2024-04-05 15:30:08 +0300

    add files

---
+++ get_string_session.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import os
+
+import dotenv
+
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+load_dotenv()
+
+with TelegramClient(StringSession(), os.getenv("TELEGRAM_API_ID"), os.getenv("TELEGRAM_API_HASH")) as client:
+    print(client.session.save())
+    # Write changes to .env file. DO NOT TEST need testing :)
+    os.environ["TELEGRAM_STRING_SESSION"] = client.session.save()
+    dotenv.set_key(os.environ["TELEGRAM_STRING_SESSION"])
