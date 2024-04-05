050deb62e88cf27fbf3ca670a60a8bad2c578ac0
Your Name <you@example.com>
2024-04-05 15:47:11 +0300

    del files add config

--- test222.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://api.elphen.site/api?mode=default_my_poolkey")
-if response.status_code == 200:
-    FreeAI_Pool_Token=response.json()
-
-print(FreeAI_Pool_Token)
