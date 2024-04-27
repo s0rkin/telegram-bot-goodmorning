46a771240d58a386412eadf2dfa4937eedfc89c7
Your Name <you@example.com>
2024-04-27 17:45:48 +0300

    FIX: fix write .env file and testing it.

--- get_string_session.py
+++ get_string_session.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python3
 
 import os
-
-from dotenv import load_dotenv, set_key
+import dotenv
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-load_dotenv()
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
 
 with TelegramClient(StringSession(), os.getenv("TELEGRAM_API_ID"), os.getenv("TELEGRAM_API_HASH")) as client:
     print(client.session.save())
-    # Write changes to .env file. DO NOT TEST need testing :)
+    # Write changes to .env file.
     os.environ["TELEGRAM_STRING_SESSION"] = client.session.save()
-    set_key(os.environ["TELEGRAM_STRING_SESSION"])
+    dotenv.set_key(dotenv_file, "TELEGRAM_STRING_SESSION", os.environ["TELEGRAM_STRING_SESSION"])
